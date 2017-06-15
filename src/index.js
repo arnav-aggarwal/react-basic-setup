18339a29c7078347a42ea317a340e426ac4c18d5
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-06-14 20:13:42 -0700

    Abstract div away into another file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
-const myDiv = <div>This is JSX!</div>;
 const container = document.getElementById('container');
-
-ReactDOM.render(myDiv, container);
+ReactDOM.render(<App />, container);
